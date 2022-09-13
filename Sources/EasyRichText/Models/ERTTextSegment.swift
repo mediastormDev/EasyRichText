dcc4516ae9b0e17f5252c5a70d229e79d7dce859
Shibo Lyu <github@of.sb>
2022-09-13 17:04:48 +0800

    doc: JSOn keys.

--- Sources/EasyRichText/Models/ERTTextSegment.swift
+++ Sources/EasyRichText/Models/ERTTextSegment.swift
@@ -11,25 +11,45 @@ import Foundation
 public struct ERTTextSegment: Codable {
     /// Only basic font styles.
     public enum Style: String, Codable {
+        /// `bd`
         case bold = "bd"
+        /// `it`
         case italic = "it"
+        /// `ul`
         case underline = "ul"
+        /// `st`
         case strikethrough = "st"
+        /// `ms`
         case monospace = "ms"
     }
     
     /// Colors are predefined, so they can be optimized for color schemes.
     public enum Color: String, Codable {
+        /// `rd`
         case red = "rd"
+        /// `yl`
         case yellow = "yl"
+        /// `bl`
         case blue = "bl"
+        /// `gr`
         case green = "gr"
+        /// `pp`
         case purple = "pp"
+        /// `pk`
         case pink = "pk"
     }
     
+    /// The text in current segment.
+    ///
+    /// JSON key:  `t`.
     var text: String
+    /// The style modifier.
+    ///
+    /// JSON key: `s`.
     var style: Set<Style>?
+    /// The color of the font. `nil` for standard body color.
+    ///
+    /// JSON key: `c`.
     var color: Color?
     
     enum CodingKeys: String, CodingKey {
