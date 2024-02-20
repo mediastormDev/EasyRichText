c717240b918ce5e4efeb9cef014168fe2fb22c6b
Shibo Lyu <github@of.sb>
2024-02-20 17:12:11 +0800

    doc: Fix doc build process.

--- Scripts/Build_Doc.sh
+++ Scripts/Build_Doc.sh
@@ -1,15 +1,16 @@
 xcodebuild docbuild -scheme EasyRichText-Package -destination 'platform=iOS Simulator,name=iPhone 15' -parallelizeTargets -derivedDataPath docsData
-echo "Copying DocC archives to doc_archives..."
-mkdir docArchives
+echo "Copying DocC archives to docArchives..."
+mkdir -p docArchives
 cp -R `find docsData -type d -name "*.doccarchive"` docArchives
 
+cd docArchives
 # Loop over all directories ending with ".doccarchive"
-for dir in docArchives/*/; do
+for dir in */; do
     if [[ "$dir" == *.doccarchive/ ]]; then
         ARCHIVE=${dir%/} # Remove trailing slash
         ARCHIVE_NAME=${ARCHIVE%.doccarchive}
-        echo "Processing archive: $ARCHIVE_NAME"
+        echo "Processing archive: $ARCHIVE_NAME ($ARCHIVE)"
 
-        $(xcrun --find docc) process-archive transform-for-static-hosting "$ARCHIVE" --hosting-base-path $ARCHIVE_NAME --output-path ./public/$ARCHIVE_NAME
+        $(xcrun --find docc) process-archive transform-for-static-hosting "$ARCHIVE" --hosting-base-path $ARCHIVE_NAME --output-path ../public/$ARCHIVE_NAME
     fi
 done
