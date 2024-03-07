07d0f6c7bf2e484493c58efedee21eed170af021
Shibo Lyu <github@of.sb>
2024-03-07 19:13:20 +0800

    fix(ios): Width.

--- Sources/EasyRichTextUI/ERTTextEditor_UIKit.swift
+++ Sources/EasyRichTextUI/ERTTextEditor_UIKit.swift
@@ -10,21 +10,43 @@ import SwiftUI
 import UIKit
 import EasyRichText
 
-public struct ERTTextEditor<RichText: ERTRichText>: UIViewRepresentable {
-    @ObservedObject public var editContext: ERTRichTextEditContext<RichText>
-    var customize: ((UITextView) -> ())?
-
-    public init(editContext: ERTRichTextEditContext<RichText>, customize: ((UITextView) -> ())? = nil) {
-        self.editContext = editContext
-        self.customize = customize
+// https://gonzalezreal.github.io/2020/01/31/rendering-attributed-strings-in-swiftui.html
+private final class TextView: UITextView {
+    var maxLayoutWidth: CGFloat = 0 {
+        didSet {
+            guard maxLayoutWidth != oldValue else { return }
+            invalidateIntrinsicContentSize()
+        }
     }
 
-    public func makeCoordinator() -> ERTTextViewDelegate<RichText> {
+    override var intrinsicContentSize: CGSize {
+        guard maxLayoutWidth > 0 else {
+            return super.intrinsicContentSize
+        }
+
+        return sizeThatFits(
+            CGSize(width: maxLayoutWidth, height: .greatestFiniteMagnitude)
+        )
+    }
+}
+
+private struct ERTTextEditorRaw<RichText: ERTRichText>: UIViewRepresentable {
+    @ObservedObject public var editContext: ERTRichTextEditContext<RichText>
+    var customize: ((UITextView) -> ())?
+    var maxLayoutWidth: CGFloat
+
+    init(editContext: ERTRichTextEditContext<RichText>, customize: ((UITextView) -> ())? = nil, maxLayoutWidth: CGFloat) {
+        self.editContext = editContext
+        self.customize = customize
+        self.maxLayoutWidth = maxLayoutWidth
+    }
+
+    func makeCoordinator() -> ERTTextViewDelegate<RichText> {
         .init(context: editContext)
     }
 
-    public func makeUIView(context: Context) -> UITextView {
-        let textView = UITextView()
+    func makeUIView(context: Context) -> TextView {
+        let textView = TextView()
 
         editContext.onTextUpdated = { newText in
             let selection = textView.selectedTextRange
@@ -41,11 +63,33 @@ public struct ERTTextEditor<RichText: ERTRichText>: UIViewRepresentable {
 
         customize?(textView)
 
+        textView.maxLayoutWidth = maxLayoutWidth
+
         return textView
     }
 
-    public func updateUIView(_ uiView: UITextView, context: Context) {
+    func updateUIView(_ uiView: TextView, context: Context) {
+        uiView.maxLayoutWidth = maxLayoutWidth
+    }
+}
 
+public struct ERTTextEditor<RichText: ERTRichText>: View {
+    @ObservedObject public var editContext: ERTRichTextEditContext<RichText>
+    var customize: ((UITextView) -> ())?
+
+    public init(editContext: ERTRichTextEditContext<RichText>, customize: ((UITextView) -> ())? = nil) {
+        self.editContext = editContext
+        self.customize = customize
+    }
+
+    public var body: some View {
+        GeometryReader { geometry in
+            ERTTextEditorRaw(
+                editContext: editContext,
+                customize: customize,
+                maxLayoutWidth: geometry.size.width - geometry.safeAreaInsets.leading - geometry.safeAreaInsets.trailing
+            )
+        }
     }
 }
 #endif
